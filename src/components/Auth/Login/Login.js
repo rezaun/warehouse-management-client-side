a2be5c5d083726d907d17c7497b20a56402e4092
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-08-04 06:35:31 +0600

    blog number 4 question answer has been added

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -139,17 +139,12 @@ const Login = () => {
                                     <p className="text-center font-semibold mx-4 mb-0">OR</p>
                                 </div>
 
-                                <a
-                                    className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
-                                    style={{ backgroundColor: "rgb(36 27 27)" }}
-                                    href="#!"
+                                <button className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center" style={{ backgroundColor: "rgb(36 27 27)" }}href="#!"
                                     role="button"
                                     data-mdb-ripple="true"
                                     data-mdb-ripple-color="light"
-                                onClick={()=>signInWithGoogle()} >
-                                    {/* <!-- Twitter --> */}
-                                    <img className='w-6 pr-2' src={google} alt=""  /> Continue with Google
-                                </a>
+                                onClick={()=>signInWithGoogle()} >                                    
+                                    <img className='w-6 pr-2' src={google} alt=""/> Continue with Google</button>
 
                             </form>
                         </div>
