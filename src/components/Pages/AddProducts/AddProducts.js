852d22a7ea073d57a8085d3cf68e88c8364c0409
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-09-06 03:04:59 +0600

    single product adding from client site

--- src/components/Pages/AddProducts/AddProducts.js
+++ src/components/Pages/AddProducts/AddProducts.js
@@ -3,27 +3,44 @@ import { useForm } from "react-hook-form";
 
 const AddProducts = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data);
+        const url =`http://localhost:5000/product`;
+        fetch(url, {
+            method : 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+        .then(res=>res.json())
+        .then(result => {
+            console.log(result);
+        })
+    }
+    
     return (
         <div className='container'>
             <div className="row">
                 <div className='w-50 mx-auto'>
-                    <h2>Please Add Product</h2>
+                    <h2 className='fs-2 text-center py-3'>Please Add Product</h2>
                     <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
                         {/* register your input into the hook by invoking the "register" function */}
-                        <input className='p-1 border border-dark mb-3 rounded' defaultValue="name" {...register("example")} />
+                        <input className='p-1 border border-dark mb-3 rounded' placeholder='Name' {...register("name", { required: true })} />
 
                         {/* include validation with required or other standard HTML validation rules */}
-                        <input className='p-1 mb-3 border border-dark rounded' {...register("price", { required: true })} />
-                        <input className='p-1 mb-3 border border-dark rounded' {...register("quantity", { required: true })} />
-                        <input className='p-1 mb-3 border border-dark rounded' {...register("supplier", { required: true })} />
-                        <input className='p-1 mb-3 border border-dark rounded' {...register("phone", { required: true })} />
-                        <input className='p-1 mb-3 border border-dark rounded' {...register("Email", { required: true })} />
-                        <textarea className='p-1 mb-3 border border-dark rounded' {...register("Description", { required: true })} />
+                        <input className='p-1 mb-3 border border-dark rounded' placeholder='Price' {...register("price", { required: true })} />
+                        <input className='p-1 mb-3 border border-dark rounded' placeholder='Quantity' {...register("quantity", { required: true })} />
+                        <input className='p-1 mb-3 border border-dark rounded' placeholder='Supplier' {...register("supplier", { required: true })} />
+                        <input className='p-1 mb-3 border border-dark rounded' placeholder='Phone' {...register("phone", { required: true })} />
+                        <input className='p-1 mb-3 border border-dark rounded' placeholder='Email' {...register("email", { required: true })} />
+                        <input className='p-1 mb-3 border border-dark rounded' placeholder='Image' {...register("image", { required: true })} />
+                        <input className='p-1 mb-3 border border-dark rounded' placeholder='Company' {...register("company", { required: true })} />
+                        <textarea className='p-1 mb-3 border border-dark rounded' placeholder='Description' {...register("description", { required: true })} />
                         {/* errors will return when field validation fails  */}
                         {errors.exampleRequired && <span>This field is required</span>}
 
-                        <input type="submit" />
+                        <input className='btn btn-warning' value="Add Products" type="submit" />
                     </form>
                 </div>
             </div>
